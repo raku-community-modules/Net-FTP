0fe92c2156b711575d38222f402441fe9838e517
araraloren <blackcatoverwall@gmail.com>
2015-10-27 00:42:25 +0800

    add stor still have some problem

--- lib/Net/Ftp.pm
+++ lib/Net/Ftp.pm
@@ -12,21 +12,19 @@ enum TYPE is export < A I >;
 enum FILE is export < NORMAL DIR LINK SOCKET PIPE CHAR BLOCK >;
 
 has $!SOCKET_CLASS = IO::Socket::INET;
-
 has $!ftpc;
 has $!ftpd;
 has $!res;
 has $!msg;
-has	$.family = 2; #3 -> IPV6
-
+has $.family = 2; #3 -> IPV6
 has Str $.host = "";
 has Int $.port = 21;
 has Str $.user = "";
 has Str $.pass = "";
-
 has Bool $.debug = False;
 has Bool $.pasv = False;
 has Bool $.ascii = True;
+has @!cbuff;
 
 method new (*%args is copy) {
 	self.bless(|%args);
@@ -103,10 +101,13 @@ method pwd() {
 # cause of perl6's getsockname not implement, you need
 # pass a port to method
 method port(Int $port?) {
+# 200 ok
+# 500|501|530 error
 # server does not connect to us immediately
 	return FTP::FAIL if $!pasv;
 	$!pasv = False;
     if ($port.defined) {
+    	return FTP::FAIL;
 		#OS::gethost not implement now ..
     } else {
     	return FTP::FAIL;
@@ -114,10 +115,17 @@ method port(Int $port?) {
 }
 
 method pasv() {
-	$!pasv = True; 
+	self!sendcmd1('PASV');
+	unless self!handlecmd() {
+		return FTP::FAIL;
+	}
+	$!pasv = True;
+	return FTP::OK; 
 }
 
 method type(TYPE $t) {
+# 200 ok
+# 500|501|504|530 error
 	given $t {
 		when TYPE::A {
 			unless $!ascii {
@@ -142,7 +150,17 @@ method type(TYPE $t) {
 	return FTP::OK;
 }
 
+method ascii() {
+	self.type(TYPE::A);
+}
+
+method binary() {
+	self.type(TYPE::I);
+}
+
 method rest($pos) {
+# 350 ok
+# 500|510|502|530 error
 	self!sendcmd2('REST', ~$pos);
 	unless self!handlecmd() {
 		return FTP::FAIL;
@@ -151,10 +169,14 @@ method rest($pos) {
 }
 
 method list($path?) {
-	if $!pasv {
-		self!pasv_connect(:line-separator("\r\n"));
-	} else {
-		self!port_connect();
+# 125|150 ok
+# 226|250 ok
+# 425|426|451 error
+# 450|500|501|502|530 error
+	unless $!pasv ?? 
+			self!pasv_connect(:line-separator("\r\n")) !!
+			self!port_connect() {
+		return ();
 	}
 	if $path.defined {
 		self!sendcmd2('LIST', ~$path);
@@ -162,10 +184,13 @@ method list($path?) {
 		self!sendcmd1('LIST');
 	}
 	my @res = ();
-	if self!handlecmd() & self!handlecmd() {
-		@res = self!readlist();	
+	if self!handlecmd() {
+		@res = self!readlist();
+		$!ftpd.close();	
+		if $!res != 226 && $!res != 250 {
+			self!handlecmd();
+		}
 	}
-	$!ftpd.close() if $!ftpd;
 	return @res;
 }
 
@@ -185,6 +210,69 @@ method dir($path?) {
 	}
 }
 
+multi method stor(Str $path, Buf $data) {
+# 150|125 ok
+# (110)
+# 226|250 ok
+# 425|426|451|551|552 error
+# 532|450|452|553 error
+# 500|501|530 error
+	return FTP::FAIL if $!ascii;
+	self.binary();
+	$path.subst('\012', '\000');	
+	self!sendcmd2('STOR', $path);
+	if self!handlecmd() {
+		unless $!pasv ?? 
+				self!pasv_connect() !!
+				self!port_connect() {
+			return FTP::FAIL;
+		}
+		$!ftpd.write: $data;
+		$!ftpd.close();
+		if $!res != 226 && $!res != 250 {
+			if self!handlecmd() {
+				return FTP::OK;
+			}
+		}
+		$!ftpd.close();
+	}
+	return FTP::FAIL;
+}
+
+multi method stor(Str $path, Str $data) {
+# 150|125 ok
+# (110)
+# 226|250 ok
+# 425|426|451|551|552 error
+# 532|450|452|553 error
+# 500|501|530 error
+	return FTP::FAIL if ! $!ascii;
+	unless $!pasv ?? 
+			self!pasv_connect() !!
+			self!port_connect() {
+		return FTP::FAIL;
+	}	
+	self!sendcmd2('STOR', $path);
+	if self!handlecmd() {
+		$!ftpd.print($data ~ "\r\n");
+		$!ftpd.close();
+		if $!res != 226 && $!res != 250 {
+			if self!handlecmd() {
+				return FTP::OK;
+			}
+		}
+	}
+	return FTP::FAIL;
+}
+
+multi method put(Str $path, Bool $over-write = False) {
+
+}
+
+multi method put(Str $path, Str $remote-path, Bool $over-write = False) {
+
+}
+
 method res() {
 	$!res;
 }
@@ -195,24 +283,26 @@ method msg() {
 
 method !readlist() {
 	my @infos;
-
-	while (my $buf = $!ftpd.get()) {
-		note $buf if $!debug;
-		$buf.chomp;
-		if $buf ~~ /^\+/ {
-			push @infos, self!eplf(~$buf);
-		} else {
-			push @infos, self!binls(~$buf);
+	
+	while (my $buf = $!ftpd.recv(:bin)) {
+		for self!buflines($buf) {
+			my $str = $_.unpack("A*");
+			
+			note $str if $!debug;
+			
+			if $str ~~ /^\+/ {
+				push @infos, self!eplf($str);
+			} else {
+				push @infos, self!binls($str);
+			}
 		}
-	}	
+	}
 	@infos;
 }
 
-method !pasv_connect(:$encoding = 'utf-8', :$line-separator = '\n') {
+method !pasv_connect(:$encoding = 'utf-8', :$line-separator = "\n") {
 # 227 ok
 # 500|501|502|530 error
-	self!sendcmd1('PASV');
-	self!respone();
 	if ($!msg ~~ /(\d+\,\d+\,\d+\,\d+)\,(\d+)\,(\d+)/) {
 		$!ftpd = $!SOCKET_CLASS.new(
 						:host($0.split(',').join('.')), 
@@ -230,7 +320,7 @@ method !pasv_connect(:$encoding = 'utf-8', :$line-separator = '\n') {
 }
 
 method !port_connect() {
-	#
+	#return FTP::FAIL;
 }
 
 method !connect($h, $p, $f) {
@@ -288,27 +378,33 @@ method !handlecmd() {
 }
 
 method !respone() {
-	my $line = $!ftpc.get();
+	my ($buf, $res, $msg, $line);
 	
-	note ~$line if $!debug;
-	
-	if ($line ~~ /^(\d ** 3)\s(.*)/) {
-		($!res, $!msg) = ($0, $1);
-	} elsif ($line ~~ /^(\d ** 3)\-(.*)/) {
-		my ($res, $msg) = ($0, $1);
+	loop (;;) {
+		if +@!cbuff {
+			$line =@!cbuff.shift;
 		
-		loop {
-			$line = $!ftpc.get();
-			
-			if ($line ~~ /^$res\s(.*)/) {
+			if ($line ~~ /^(\d ** 3)\s(.*)/) {
+				($!res, $!msg) = ($0, $1);
+				last;
+			} elsif ($line ~~ /^(\d ** 3)\-(.*)/) {
+				my ($res, $msg) = ($0, $1);
+			} elsif ($line ~~ /^$res\s(.*)/) {
 				($!res, $!msg) = ($res, $msg ~ $0);
 				last;
 			} else {
-				$msg = $msg ~ $line;
+				($!res, $!msg) = (-1, 'Unknow respone!');
+				last;
+			}
+		} else {
+			$buf = $!ftpc.recv(:bin);
+			note $buf;
+			for self!buflines($buf) {
+				$line = $_.unpack("A*");
+				@!cbuff.push: $line;
+				{ note '+' ~ $line ~ '+' } if $!debug; 
 			}
 		}
-	} else {
-		($!res, $!msg) = (-1, 'Unknow respone!');
 	}
 }
 
@@ -330,7 +426,8 @@ method !dispatch() {
 			 530 | 500 | 501 | 503 | 421 |
 			 502 | 550 |
 			 504 |
-			 425 | 426 | 451 | 450 {
+			 425 | 426 | 451 | 450 |
+			 551 | 552 | 553 | 532 | 452 {
 			return FTP::FAIL;
 		}
 	}
@@ -487,5 +584,34 @@ method !binls(Str $str is copy) {
 	%info;
 }
 
+method !buflines(Buf $buf, Bool :$empty = False) {
+	my @ret;
+	
+	my Int $l = 0;my Int $r = 0;
+	
+	my $len = $buf.elems - 1;
+	
+	while ($r < $len) {
+		if $buf[$r] == 13 &&
+			$buf[$r + 1] == 10 {
+			if ($l - $r) || $empty {
+				@ret.push: $buf.subbuf($l, $r - $l);
+			}
+			$r += 2;
+			$l = $r;
+		} else {
+			$r++;
+		}
+	}
+	
+	if ($l - $r) {
+		@ret.push: $buf.subbuf($l, $r - $l);
+	}
+	
+	@ret;
+}
+
 #NOT IMPLEMENT
-# STRU MODE ALLO [TIMEOUT]
+# STRU MODE ALLO [TIMEOUT] 
+#WILL NOT IMPLEMENT
+# NLIST
